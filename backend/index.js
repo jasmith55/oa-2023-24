b080241a037c65f978a614443d061069006d2e5e
jasmith55 <jasmith55@wisc.edu>
2023-09-17 12:10:25 -0500

    Updated index.js

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 import express from "express"
 import cors from "cors"
 import fs from "fs";
-import csv from "csv-parser";
+import readline from "readline";
 
 const app = express()
 
@@ -11,20 +11,37 @@ app.use(cors({
 
 app.get("/", (req, res) => {
     const data = [];
-    fs.createReadStream("data.csv")
-        .pipe(csv())
-        .on("data", (row) => {
-            data.push(row);
-        })
-        .on("end", () => {
-            // Send the parsed data as a JSON response
-            res.json(data);
-        })
-        .on("error", (error) => {
-            console.error("Error reading CSV file:", error);
-            res.status(500).json({ error: "Internal Server Error" });
-        });
-})
+    const filePath = "data.csv";
+
+    // Create a readable stream to read the CSV file line by line
+    const rl = readline.createInterface({
+        input: fs.createReadStream(filePath),
+        output: process.stdout,
+        terminal: false
+    });
+
+    rl.on("line", (line) => {
+        // Split each line into an array using a comma as the delimiter
+        const values = line.split(",");
+        
+        // Assuming the first value is 'x' and the second is 'y'
+        const x = parseFloat(values[0]);
+        const y = parseFloat(values[1]);
+
+        // Push the data to the array
+        data.push({ x, y });
+    });
+
+    rl.on("close", () => {
+        // Send the parsed data as a JSON response
+        res.json(data);
+    });
+
+    rl.on("error", (error) => {
+        console.error("Error reading CSV file:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    });
+});
 
 
 const port = process.env.PORT || 3000
